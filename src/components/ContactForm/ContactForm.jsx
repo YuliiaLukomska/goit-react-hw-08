486f21c7ba170847004a848fdafcbb092c9177ae
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-04-14 19:04:25 +0300

    add hotToast

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,10 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import css from "./ContactForm.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addContact } from "../../redux/contacts/operations";
+import toast, { Toaster } from "react-hot-toast";
+import { selectContacts } from "../../redux/contacts/selectors";
 
 const ContactSchema = Yup.object().shape({
   name: Yup.string()
@@ -22,40 +24,64 @@ const INITIAL_FORM_DATA = {
 
 const ContactForm = () => {
   const dispatch = useDispatch();
+  const currentContactsArray = useSelector(selectContacts);
 
   const handleSubmit = (data, formActions) => {
+    const isAlreadyAdded = currentContactsArray.find(
+      (item) =>
+        item.name.toLowerCase() === data.name.toLowerCase() ||
+        item.number === data.number
+    );
+    if (isAlreadyAdded) {
+      toast("This contact is already added to your contacts!");
+      return;
+    }
     dispatch(addContact(data));
     formActions.resetForm();
   };
 
   return (
-    <Formik
-      initialValues={INITIAL_FORM_DATA}
-      onSubmit={handleSubmit}
-      validationSchema={ContactSchema}
-    >
-      <Form className={css.formWrapper}>
-        <label className={css.formLabel}>
-          Name
-          <Field name="name" type="text" placeholder="Maria Moroz" />
-          <ErrorMessage name="name" component="span" className={css.error} />
-        </label>
-        <label className={css.formLabel}>
-          Number
-          <Field name="number" type="text" placeholder="+(380)631234567" />
-          <ErrorMessage name="number" component="span" className={css.error} />
-        </label>
+    <>
+      <Toaster
+        toastOptions={{
+          style: {
+            background: "pink",
+            color: "black",
+          },
+        }}
+      />
+      <Formik
+        initialValues={INITIAL_FORM_DATA}
+        onSubmit={handleSubmit}
+        validationSchema={ContactSchema}
+      >
+        <Form className={css.formWrapper}>
+          <label className={css.formLabel}>
+            Name
+            <Field name="name" type="text" placeholder="Maria Moroz" />
+            <ErrorMessage name="name" component="span" className={css.error} />
+          </label>
+          <label className={css.formLabel}>
+            Number
+            <Field name="number" type="text" placeholder="+(380)631234567" />
+            <ErrorMessage
+              name="number"
+              component="span"
+              className={css.error}
+            />
+          </label>
 
-        <button
-          className={css.formBtn}
-          type="submit"
-          title="Click to create new contact"
-          aria-label="Add new contact"
-        >
-          Add new contact
-        </button>
-      </Form>
-    </Formik>
+          <button
+            className={css.formBtn}
+            type="submit"
+            title="Click to create new contact"
+            aria-label="Add new contact"
+          >
+            Add new contact
+          </button>
+        </Form>
+      </Formik>
+    </>
   );
 };
 
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -5,12 +5,14 @@ import {
   postContact,
   removeContact,
 } from "../../services/contactsServices";
+import toast from "react-hot-toast";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
       const data = await getContacts();
+
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -23,6 +25,7 @@ export const addContact = createAsyncThunk(
   async (newContact, thunkAPI) => {
     try {
       const data = await postContact(newContact);
+      toast("Your contact has been successfully added!");
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -35,6 +38,7 @@ export const deleteContact = createAsyncThunk(
   async (contactId, thunkAPI) => {
     try {
       const data = await removeContact(contactId);
+      toast("Your contact has been successfully deleted!");
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
