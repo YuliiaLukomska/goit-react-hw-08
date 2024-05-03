a29210931bc56b2a4c2ab2dd188078a43bb6ce5e
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-05-03 15:18:05 +0300

    add styles to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,10 @@
 import DocumentTitle from "../DocumentTitle";
 import css from "../components/PagesStyles/HomePage.module.css";
+import { MdAccountBox } from "react-icons/md";
+import { IoPersonAddOutline } from "react-icons/io5";
+import { GrEdit } from "react-icons/gr";
+import { RiFilter2Fill } from "react-icons/ri";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const Home = () => {
   return (
@@ -12,10 +17,39 @@ const Home = () => {
             💁‍♀️
           </span>
         </h1>
-        <p className={css.text}>
-          Here you can create and save your private contacts, edit them and use
-          the filter to make your search easier.
-        </p>
+        <p className={css.text}>How to use this application effectively</p>
+        <ul>
+          <li>
+            <p className={css.text}>
+              <MdAccountBox />
+              Create your account
+            </p>
+          </li>
+          <li>
+            <p className={css.text}>
+              <IoPersonAddOutline />
+              Add and save your private contacts
+            </p>
+          </li>
+          <li>
+            <p className={css.text}>
+              <GrEdit />
+              Edit your contacts
+            </p>
+          </li>
+          <li>
+            <p className={css.text}>
+              <RiFilter2Fill />
+              Use the filter to make your search easier
+            </p>
+          </li>
+          <li>
+            <p className={css.text}>
+              <RiDeleteBin6Line />
+              Delete contacts if it is necessary
+            </p>
+          </li>
+        </ul>
       </div>
     </>
   );
