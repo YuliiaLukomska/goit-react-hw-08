853ac93b47c21758778bab5d772387cb89bc075f
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-09-30 15:46:02 +0300

    update baseurl

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://connections-api.herokuapp.com",
+  baseURL: "https://connections-api.goit.global",
 });
 
 export const setToken = (token) => {
